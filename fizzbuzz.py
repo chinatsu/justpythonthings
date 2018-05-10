f73ea75a8be29e55de01d2f8da05c2edb51a976d
Kent Daleng <lolexplode@gmail.com>
2018-05-10 11:15:01 +0100

    try to clear up some comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,13 @@ def fizzbuzz(start, end):
     return [
         (
             x, # the number
+            # create a dict with all of the fizzbuzz data, and look up what lies at index x.
             dict(
                 # fizz
                 {str(x): getattr("", "join")([chr(x) for x in [70, 105, 122, 122]]) for x in range(start, end+1) if x % 3 == 0},
-                # buzz, note that we need `x % 3 != 0` as to not step on fizzbuzz' toes. (type object got multiple values for argument)
-                # something weird about the **args here, since `x % 5 != 0` is not needed in the above dict
+                # buzz, note that we need `x % 3 != 0` as to not step on fizzbuzz' toes. as far as i understand, each of the **kwargs
+                # will get expanded to essentially dict(fizz, str(5)="Buzz", str(10)="Buzz", ...), (except you can't normally have expressions as keyword)
+                # and at some point down the line, we'd have 15="Buzz" and 15="FizzBuzz" in the same list of kwargs.
                 **{str(x): getattr("", "join")([chr(x) for x in [66, 117, 122, 122]]) for x in range(start, end+1) if x % 5 == 0 and x % 3 != 0},
                 # fizzbuzz
                 **{str(x): getattr("", "join")([chr(x) for x in [70, 105, 122, 122, 66, 117, 122, 122]]) for x in range(start, end+1) if x % 3 == 0 and x % 5 == 0},
