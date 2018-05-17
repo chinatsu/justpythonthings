678d017fd1e62d92a3af85a53c96f9155d23489a
Kent Daleng <lolexplode@gmail.com>
2018-05-17 08:05:49 +0100

    only use emojis as seeds

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,13 +6,13 @@ def fizzbuzz(start, end):
             dict(
                 # fizz
                 {str(x): getattr("", "join")(
-                    getattr("{3}{2}{1}{1}", "format")(
-                        getattr(__import__('random'), 'seed')("wrong"),
+                    getattr("{2}{3}{1}{1}", "format")(
+                        getattr(__import__('random'), 'seed')("📈"),
                         chr(122),
-                        getattr(__import__('random'), "choice")(
-                            getattr("a e i o u", "split")()),
                         getattr(getattr(__import__('random'), "choice")(
-                            getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")())) for x in range(start, end+1) if x % 3 == 0},
+                            getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")(),
+                        getattr(__import__('random'), "choice")(
+                            getattr("a e i o u", "split")()))) for x in range(start, end+1) if x % 3 == 0},
                 # buzz, note that we need `x % 3 != 0` as to not step on fizzbuzz' toes. as far as i understand, each of the **kwargs
                 # will get expanded to essentially dict(fizz, str(5)="Buzz", str(10)="Buzz", ...), (except you can't normally have expressions as keyword)
                 # and at some point down the line, we'd have 15="Buzz" and 15="FizzBuzz" in the same list of kwargs.
@@ -26,17 +26,17 @@ def fizzbuzz(start, end):
                             getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")())) for x in range(start, end+1) if x % 5 == 0 and x % 3 != 0},
                 # fizzbuzz
                 **{str(x): getattr("", "join")(
-                    getattr("{3}{2}{1}{1}{4}{5}{1}{1}", "format")(
-                        getattr(__import__('random'), 'seed')("destructibility"),
+                    getattr("{5}{2}{1}{1}{4}{3}{1}{1}", "format")(
+                        getattr(__import__('random'), 'seed')("🅿️"),
                         chr(122),
                         getattr(__import__('random'), "choice")(
                             getattr("a e i o u", "split")()),
-                        getattr(getattr(__import__('random'), "choice")(
-                            getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")(),
-                        getattr(getattr(__import__('random'), "choice")(
-                            getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")(),
                         getattr(__import__('random'), "choice")(
-                                getattr("a e i o u", "split")()))) for x in range(start, end+1) if x % 3 == 0 and x % 5 == 0},
+                                getattr("a e i o u", "split")()),
+                        getattr(getattr(__import__('random'), "choice")(
+                            getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")(),
+                        getattr(getattr(__import__('random'), "choice")(
+                            getattr("b c d f g h j k l m n p q r s t v x y z", "split")()), "upper")())) for x in range(start, end+1) if x % 3 == 0 and x % 5 == 0},
                  # numbers
                 **{str(x): x for x in range(start, end+1) if x % 3 != 0 and x % 5 != 0}
             )[str(x)] # the fizzbuzz part
